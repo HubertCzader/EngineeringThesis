331b4400209386aed8ceb2c51612bc14f6da6c43
HubertCzader <hczader@student.agh.edu.pl>
2022-07-29 10:04:58 +0100

    Naprawa widoku wszystkich kluczy

--- mysite/keys/views.py
+++ mysite/keys/views.py
@@ -7,12 +7,12 @@ from .models import Key
 # Create your views here.
 class KeysView(generic.ListView):
     template_name = 'keys/all_keys.html'
-    context_object_name = 'door_keys'
+    context_object_name = 'all_keys'
 
     def get_queryset(self):
         return Key.objects.all()
 
 
 def check_key(request, key_id):
-    is_active = Key.objects.get_or_404(id=key_id).active
+    is_active = Key.objects.get(id=key_id).active
     return HttpResponse(f'{is_active}')
