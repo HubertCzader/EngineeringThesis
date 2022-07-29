53a1abb54cd9f8f2eb7aa537f72a72614a204357
HubertCzader <hczader@student.agh.edu.pl>
2022-07-29 11:49:35 +0100

    Aktualizacja ask_server tak aby zwracał plik Json

--- ask_server.py
+++ ask_server.py
@@ -1,13 +1,15 @@
-import requests
 import os
+import requests
+import json
 
 os.environ['NO_PROXY'] = '127.0.0.1'
 url = 'http://127.0.0.1:8000/keys/check/'
-ID = 'e2055800-058b-42c7-b854-beef168e7ccd'
+ID = 'e2c3d3c4-12ec-4847-a1f3-e49c0c3d5b47'
 
 
 def check_if_active(key_id):
-    return bool(requests.get(url + key_id).text)
+    response = json.dumps({"active":  bool(requests.get(url + key_id).text)})
+    return json.loads(response)
 
 
 if __name__ == "__main__":
